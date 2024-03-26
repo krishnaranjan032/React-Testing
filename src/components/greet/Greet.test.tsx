176101cb957b59bfe6d0839daefb948078df3370
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-26 12:32:03 +0530

    group test using describe methods

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -5,16 +5,51 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // test.only("Greet renders with a name", () => {
+  // test.skip("Greet renders with a name", () => {
+  test("renders with a name", () => {
+    render(<Greet name="Krishna" />);
+    const textElement = screen.getByText("Hello Krishna");
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-// test.only("Greet renders with a name", () => {
-// test.skip("Greet renders with a name", () => {
-test("Greet renders with a name", () => {
-  render(<Greet name="Krishna" />);
-  const textElement = screen.getByText('Hello Krishna');
-  expect(textElement).toBeInTheDocument();
-});
+// we can also nest describe method
+// describe("Greet", () => {
+//   test("renders correctly", () => {
+//     render(<Greet />);
+//     const textElement = screen.getByText(/hello/i);
+//     expect(textElement).toBeInTheDocument();
+//   });
+
+//   // test.only("Greet renders with a name", () => {
+//   // test.skip("Greet renders with a name", () => {
+//   describe("Nested", () => {
+//     test("renders with a name", () => {
+//       render(<Greet name="Krishna" />);
+//       const textElement = screen.getByText("Hello Krishna");
+//       expect(textElement).toBeInTheDocument();
+//     });
+//   });
+// });
+
+
+// we can also have several describe methods in a single file
+describe('first', () => { test("renders with a name", () => {
+    render(<Greet name="Krishna" />);
+    const textElement = screen.getByText("Hello Krishna");
+    expect(textElement).toBeInTheDocument();
+  }); })
+
+  describe('second', () => { test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  }); })
