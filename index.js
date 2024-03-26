0f7e0adaf3bdc1b212890efe98067dc73bb83ca2
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-26 11:23:34 +0530

    setup the project setup

--- index.js
+++
@@ -1,9 +0,0 @@
-const getFullname = (fname, lname) => {
-  return `${fname} ${lname}`;
-};
-
-const actualFullname = getFullname("Krishna", "Ranjan");
-const expectedFullname = "KrishnaRanjan";
-if (actualFullname !== expectedFullname) {
-  throw new Error(`${actualFullname} is not equal to ${expectedFullname}`);
-}
