63e435b619d7515fba840fc11930d7cbeda215b9
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-26 18:04:46 +0530

    query multiple elements

---
+++ src/components/skills/Skills.tsx
@@ -0,0 +1,16 @@
+import { SkillsProps } from './Skills.types'
+
+export const Skills = (props: SkillsProps) => {
+  const { skills } = props
+
+  
+  return (
+    <>
+      <ul>
+        {skills.map((skill) => {
+          return <li key={skill}>{skill}</li>
+        })}
+      </ul>
+    </>
+  )
+}
