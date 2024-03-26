a34383b9dff8639d9258c64f5eff4f5e3707bc49
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-26 15:20:14 +0530

    will test getBy features

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -5,23 +5,6 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-describe("Greet", () => {
-  test("renders correctly", () => {
-    render(<Greet />);
-    const textElement = screen.getByText(/hello/i);
-    expect(textElement).toBeInTheDocument();
-  });
-
-  // test.only("Greet renders with a name", () => {
-  // test.skip("Greet renders with a name", () => {
-  test("renders with a name", () => {
-    render(<Greet name="Krishna" />);
-    const textElement = screen.getByText("Hello Krishna");
-    expect(textElement).toBeInTheDocument();
-  });
-});
-
-// we can also nest describe method
 // describe("Greet", () => {
 //   test("renders correctly", () => {
 //     render(<Greet />);
@@ -31,14 +14,31 @@ describe("Greet", () => {
 
 //   // test.only("Greet renders with a name", () => {
 //   // test.skip("Greet renders with a name", () => {
-//   describe("Nested", () => {
-//     test("renders with a name", () => {
-//       render(<Greet name="Krishna" />);
-//       const textElement = screen.getByText("Hello Krishna");
-//       expect(textElement).toBeInTheDocument();
-//     });
+//   test("renders with a name", () => {
+//     render(<Greet name="Krishna" />);
+//     const textElement = screen.getByText("Hello Krishna");
+//     expect(textElement).toBeInTheDocument();
 //   });
 // });
+
+// we can also nest describe method
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // test.only("Greet renders with a name", () => {
+  // test.skip("Greet renders with a name", () => {
+  describe("Nested", () => {
+    test("renders with a name", () => {
+      render(<Greet name="Krishna" />);
+      const textElement = screen.getByText("Hello Krishna");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
 
 // we can also have several describe methods in a single file
 // describe("first", () => {
