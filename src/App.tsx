9005c5257025c79392b7a70ca424ff5113866fbe
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-27 15:40:03 +0530

    make use of provider for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-
-import './App.css';
-import { Application } from './components/application/Application';
-import {Counter} from './components/counter/Counter';
-import { Skills } from './components/skills/Skills';
+import './App.css'
+import { Application } from './components/application/Application'
+// import { CounterTwo } from './components/counter-two/CounterTwo'
+import { Counter } from './components/counter/Counter'
+import { MuiMode } from './components/mui/MuiMode'
+import { Skills } from './components/skills/Skills'
+// import { Users } from './components/users/Users'
+import { AppProviders } from './providers/AppProviders'
 
 function App() {
   return (
-    <div className="App">
+    <AppProviders>
+      <div className="App">
         <Application />
         <Skills skills={['HTML', 'CSS']} />
         <Counter />
+        {/* <CounterTwo count={1} /> */}
+        {/* <Users /> */}
+        <MuiMode />
       </div>
-  );
+    </AppProviders>
+  )
 }
 
-export default App;
+export default App
